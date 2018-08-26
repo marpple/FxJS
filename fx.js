a2fb1426ee68e50188c96f179c745011ec904b42
indongyoo <idy@marpple.com>
2018-08-27 00:24:38 +0900

    init

--- fx.js
+++ fx.js
@@ -166,4 +166,5 @@ function *range(limit) {
   while (++i < limit) yield i;
 }
 
-export const object = coll => reduce((obj, [k, v]) => (obj[k] = v, obj),  coll, {});
+export const
+  object = coll => reduce((obj, [k, v]) => (obj[k] = v, obj),  coll, {});
