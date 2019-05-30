4d73714e1e87d91007647f360e0ef4d8991cd495
dohyeong <dhl@marpple.com>
2019-05-30 14:46:09 +0900

    L.intersectionBy함수 코드 정리

--- Lazy/intersectionByLazy.js
+++ Lazy/intersectionByLazy.js
@@ -1,9 +1,8 @@
 import curry from "../curry.js";
-import go from '../go.js';
 import mapLazy from './mapLazy.js';
 import filterLazy from './filterLazy.js';
 
 export default curry(function *intersectionByLazy(f, a, b) {
-  const setB = go(b, mapLazy(f), it => new Set(it));
+  const setB = new Set(mapLazy(f, b));
   yield* new Set(filterLazy(item => setB.has(f(item)), a));
 });
