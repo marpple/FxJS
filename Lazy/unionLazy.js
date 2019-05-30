fca9ce73593f39a9f24b7f03ac6430d016e9fcaf
dohyeong <dhl@marpple.com>
2019-05-30 14:47:58 +0900

    unionLazy 유명함수로 작성

--- Lazy/unionLazy.js
+++ Lazy/unionLazy.js
@@ -2,6 +2,6 @@ import curry from '../curry.js';
 import identity from '../identity.js';
 import unionByLazy from './unionByLazy.js';
 
-export default curry(function(a, b) {
+export default curry(function unionLazy(a, b) {
   return unionByLazy(identity, a, b);
 });
