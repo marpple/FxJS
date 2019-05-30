4af6d32c6127870a78d6abc4ab050653604a33ef
dohyeong <dhl@marpple.com>
2019-05-30 14:30:41 +0900

    difference함수 구현 go1로 수정

--- Lazy/differenceByLazy.js
+++ Lazy/differenceByLazy.js
@@ -1,9 +1,8 @@
 import curry from "../curry.js";
-import map from '../map.js';
+import mapLazy from './mapLazy.js';
 
 export default curry(function *differenceByLazy(f, a, b) {
-  console.log(f, a, b);
-  const set = new Set(map(f, b));
+  const set = new Set(mapLazy(f, b));
   for (const item of a) {
     if (!set.has(f(item))) yield item;
   }
--- differenceBy.js
+++ differenceBy.js
@@ -1,12 +1,11 @@
 import curry from './curry.js';
 import takeAll from './takeAll.js';
 import differenceByLazy from './Lazy/differenceByLazy.js';
-import go from './go.js';
+import go1 from './go.js';
 
 export default curry(function differenceBy(f, a, b) {
-  return go(
-    [a, b],
-    args => differenceByLazy(f, ...args),
+  return go1(
+    differenceByLazy(f, a, b),
     takeAll
   )
 });
