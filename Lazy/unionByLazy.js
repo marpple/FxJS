5dbac0187f2d8cbc08406b4a32a52a650fbc0c76
dohyeong <dhl@marpple.com>
2019-06-03 17:49:02 +0900

    union Promise지원하도록 수정

--- Lazy/unionByLazy.js
+++ Lazy/unionByLazy.js
@@ -1,13 +1,11 @@
-import curry from '../curry.js';
 import flatLazy from './flatLazy.js';
+import uniqueByLazy from './uniqueByLazy.js';
+import curry from '../curry.js';
+import go from '../go.js';
 
-export default curry(function* unionByLazy(f, a, b) {
-  const set = new Set();
-  for (const item of flatLazy([a, b])) {
-    const itemByF = f(item);
-    if (!set.has(itemByF)) {
-      set.add(itemByF);
-      yield item;
-    }
-  }
+export default curry(function unionByLazy(f, a, b) {
+  return go(
+    [a, b],
+    flatLazy,
+    uniqueByLazy(f))
 });
