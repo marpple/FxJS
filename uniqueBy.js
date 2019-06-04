7cef69c6e8ffaec3f4466661df44a9062906f785
dohyeong <dhl@marpple.com>
2019-06-04 15:49:37 +0900

    uniqueBy 코드 정리

--- uniqueBy.js
+++ uniqueBy.js
@@ -1,13 +1,13 @@
 import uniqueByLazy from "./Lazy/uniqueByLazy.js";
+import entriesLazy from './Lazy/entriesLazy.js';
 import curry from './curry.js';
 import isIterable from './isIterable.js';
 import object from './object.js';
 import takeAll from "./takeAll.js";
-import go1 from './go1.js';
+import last from './last.js';
 
 export default curry(function uniqueBy(f, iter) {
-  return go1(
-    uniqueByLazy(f, iter),
-    isIterable(iter) ? takeAll : object,
-  );
+  return isIterable(iter) ?
+    takeAll(uniqueByLazy(f, iter)) :
+    object(uniqueByLazy(e => f(last(e)), entriesLazy(iter)));
 });
