38aba23ba5f85046962d88faf34d0c320da12bce
indongyoo <idy@marpple.com>
2019-06-09 18:52:54 +0900

    groupBy 정리

--- groupBy.js
+++ groupBy.js
@@ -2,15 +2,11 @@ import curry from "./curry.js";
 import reduce from "./reduce.js";
 import go1 from "./go1.js";
 
-function pushSel(parent, k, v) {
-  (parent[k] || (parent[k] = [])).push(v);
-  return parent;
-}
-
 export default curry(function groupBy(f, iter) {
   return reduce(
-    (group, a) =>
-      go1(f(a), b => pushSel(group, b, a)),
+    (group, a) => go1(
+      f(a),
+      k => ((group[k] || (group[k] = [])).push(a), group)),
     {},
     iter);
 });
