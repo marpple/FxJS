dc02538a6a1f9b8bcda6f83eca22a7ec6ed17112
dohyeong <dhl@marpple.com>
2019-06-10 16:13:11 +0900

    dropLazy함수도 filterLazy를 활용하여 재작성

--- Lazy/dropLazy.js
+++ Lazy/dropLazy.js
@@ -1,12 +1,8 @@
 import curry from "../curry.js";
 import safety from "../safety.js";
-import nop from "../nop.js";
+import filterLazy from "./filterLazy.js";
 
-export default curry(function *dropLazy(l, iter) {
-  if (l < 1) return yield *safety(iter);
-  for (const a of safety(iter)) {
-    if (a instanceof Promise) yield a.then(a => l ? (l--, Promise.reject(nop)) : a);
-    else if (l) { l--; continue; }
-    else yield a;
-  }
+export default curry(function dropLazy(l, iter) {
+  let i = 0;
+  return filterLazy(_ => (++i) > l, safety(iter));
 });
