dffe6bf16f7440025b75291ccd53ca389f7b3ddc
dohyeong <dhl@marpple.com>
2019-06-10 16:03:21 +0900

    dropUntilLazy함수를 filterLazy함수를 활용하여 재작성

--- Lazy/dropUntilLazy.js
+++ Lazy/dropUntilLazy.js
@@ -1,17 +1,11 @@
 import curry from "../curry.js";
-import nop from "../nop.js";
 import go1 from "../go1.js";
 import safety from "../safety.js";
-import toIter from "../toIter.js";
+import filterLazy from "./filterLazy.js";
 
-export default curry(function *dropUntilLazy(f, iter) {
-  iter = toIter(safety(iter));
-  for (const a of iter) {
-    let stop = go1(a, f);
-    if (stop instanceof Promise) {
-      yield stop.then(s => (stop = s, Promise.reject(nop)));
-    }
-    if (stop) break;
-  }
-  yield* iter;
+export default curry(function dropUntilLazy(f, iter) {
+  let skip = false;
+  return filterLazy(
+    a => go1(f(a), b => skip || (skip = b, false)),
+    safety(iter));
 });
