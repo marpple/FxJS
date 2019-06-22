00ea32ae7f9d51459254e95a86cf30580fd25690
dohyeong <dhl@marpple.com>
2019-06-22 23:10:17 +0900

    코드 정리

--- Lazy/dropLazy.js
+++ Lazy/dropLazy.js
@@ -5,13 +5,12 @@ import nop from "../nop.js";
 
 export default curry(function* dropLazy(l, iter) {
   if (l < 1) yield* iter;
-  let prev = Promise.resolve();
-  let i = 0;
+  let prev = null, i = 0;
   iter = toIter(iter);
   for(const a of iter) {
     if (a instanceof Promise) {
       a.catch(noop);
-      yield prev = prev
+      yield prev = (prev || Promise.resolve())
         .then(_ => a)
         .then(b => ++i > l ? b : Promise.reject(nop));
       prev = prev.catch(noop);
