25f94a8093f4f61b696edf33dcb4155f855e8d4e
indongyoo <idy@marpple.com>
2019-07-18 19:03:41 +0900

    html 공백 제거

--- Strict/html.js
+++ Strict/html.js
@@ -4,7 +4,7 @@ import mapLazy from "../Lazy/mapLazy.js";
 
 export default function html(strs, ...datas) {
   datas = mapLazy(d => d === undefined ? '' : d, datas);
-  return reduce((res, str) =>
-    go1(datas.next().value, data => `${res}${data}${str}`),
-    strs);
+  return go1(
+    reduce((res, str) => go1(datas.next().value, data => `${res}${data}${str}`), strs),
+    a => a.replace(/\s*(\>|\<)\s*/g, '$1').trim());
 }
