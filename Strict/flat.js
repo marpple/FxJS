6139a8b8cd81cbfdc670e3603847815b1e1fdb18
indongyoo <idy@marpple.com>
2019-07-21 18:53:29 +0900

    flat depth 추가

--- Strict/flat.js
+++ Strict/flat.js
@@ -1,6 +1,6 @@
 import takeAll from "./takeAll.js";
 import flatLazy from "../Lazy/flatLazy.js";
 
-export default function flat(iter) {
-  return takeAll(flatLazy(iter));
+export default function flat(iter, depth = 1) {
+  return takeAll(flatLazy(iter, depth));
 }
