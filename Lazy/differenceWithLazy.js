39a6a01094494826153c4286a6b786955faa2ed9
dohyeong <dhl@marpple.com>
2019-07-25 20:32:00 +0900

    L.differenceWith 재작성

--- Lazy/differenceWithLazy.js
+++ Lazy/differenceWithLazy.js
@@ -1,11 +1,7 @@
 import rejectLazy from "./rejectLazy.js";
-import filterLazy from "./filterLazy.js";
 import curry from "../Strict/curry.js";
-import take from "../Strict/take.js";
-import go1 from "../Strict/go1.js";
+import some from "../Strict/some.js";
 
 export default curry(function differenceWithLazy(f, iter1, iter2) {
-  return rejectLazy(
-    a => go1(take(1, filterLazy(b => f(a, b), iter2)), b => b.length),
-    iter1);
+  return rejectLazy(a => some(b => f(a, b), iter2), iter1);
 });
