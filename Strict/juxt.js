44afb613b0d09dcea2cf933d59d5ae641eac0bda
dohyeong <dhl@marpple.com>
2019-08-13 21:04:13 +0900

    juxt함수 curry적용

--- Strict/juxt.js
+++ Strict/juxt.js
@@ -1,5 +1,6 @@
+import curry from "./curry.js";
 import map from "./map.js";
 
-export default function juxt(fns) {
-  return (...args) => map(f => f(...args), fns);
-};
+export default curry(function juxt(fns, ...args) {
+  return map(f => f(...args), fns);
+});
