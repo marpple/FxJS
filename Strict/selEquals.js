f799b238f0dbc7069d1e32a14026a608d3bfd9a4
dohyeong <dhl@marpple.com>
2019-08-13 12:42:28 +0900

    selEq, selSatisfies함수 curry2적용

--- Strict/selEquals.js
+++ Strict/selEquals.js
@@ -1,6 +1,6 @@
-import curry from "./curry.js";
+import curry2 from "./curry2.js";
 import sel from "./sel.js";
 
-export default curry(function selEquals(selector, v, obj) {
+export default curry2(function selEquals(selector, v, obj) {
   return sel(selector, obj) === v;
 });
