8830879236579e39a901f222dc167c7995ec14dc
dohyeong <dhl@marpple.com>
2019-08-15 03:42:46 +0900

    ifElse, when함수 arguments 여러 개 받도록 수정

--- Strict/ifElse.js
+++ Strict/ifElse.js
@@ -1,6 +1,6 @@
 import curry3 from "./curry3.js";
 import go1 from "./go1.js";
 
-export default curry3(function ifElse(cond, t, f, v) {
-  return go1(cond(v), b => b ? t(v) : f(v));
+export default curry3(function ifElse(cond, t, f, ...args) {
+  return go1(cond(...args), b => b ? t(...args) : f(...args));
 });
--- Strict/when.js
+++ Strict/when.js
@@ -2,6 +2,6 @@ import curry2 from "./curry2.js";
 import identity from "./identity.js";
 import ifElse from "./ifElse.js";
 
-export default curry2(function when(cond, f, v) {
-  return ifElse(cond, f, identity, v);
+export default curry2(function when(cond, f, ...args) {
+  return ifElse(cond, f, identity, ...args);
 });
