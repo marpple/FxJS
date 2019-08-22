24283bcac90b0ad61c95cee8b65c430cd07f4ecb
dohyeong <dhl@marpple.com>
2019-08-22 13:33:20 +0900

    tap함수 curry 제거

--- Strict/tap.js
+++ Strict/tap.js
@@ -1,8 +1,7 @@
-import curry from "./curry.js";
 import reduce from "./reduce.js";
-import go1Sync from "../.internal/go1Sync.js";
+import _go1 from "../.internal/go1Sync.js";
 import go1 from "./go1.js";
 
-export default curry(function tap(f, ...fs) {
-  return (a, ...as) => go1(reduce(go1Sync, f(a, ...as), fs), _ => a);
-});
+export default function tap(f, ...fs) {
+  return (a, ...as) => go1(reduce(_go1, f(a, ...as), fs), _ => a);
+};
