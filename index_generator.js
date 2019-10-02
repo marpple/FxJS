d5ae01bafe231a6032b8acb344994ff68b7445b5
dohyeong <dhl@marpple.com>
2019-10-03 01:49:23 +0900

    index.js 동적으로 생성하도록 수정

---
+++ index_generator.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+import readline from "readline";
+import { go, pipe, split, last } from "./Strict/index.js";
+
+const outputStream = fs.createWriteStream('./index.js');
+
+const write = ({ source_dir, prefix = '', postfix = '' }) =>
+  new Promise((res, rej) => {
+    const inputStream = fs.createReadStream(`${source_dir}/index.js`);
+    const rl = readline.createInterface({
+      input: inputStream
+    });
+
+    rl.on('line', line => outputStream.write(
+      line
+        .replace(/default as \w+/g, pipe(
+          split(' '),
+          last,
+          func_name => `default as ${prefix}${func_name}${postfix}`
+        ))
+        .concat('\n')
+        .replace(/\.\//, `${source_dir}/`))
+    ).on('close', _ => {
+      outputStream.write('\n');
+      res();
+    }).on('error', rej);
+  });
+
+go(
+  write({
+    source_dir: './Lazy',
+    postfix: 'L',
+    aliases: {
+      zipWithIndex: ['ipp']
+    }
+  }),
+  _ => write({
+    source_dir: './Concurrency',
+    postfix: 'C'
+  }),
+  _ => outputStream.end('export * from "./Strict/index.js";')
+);
