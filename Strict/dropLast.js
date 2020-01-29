14d137647128be251878ce501c33db01af67c551
dohyeong <dhl@marpple.com>
2020-01-29 13:47:59 +0900

    dropLast 함수 추가

---
+++ Strict/dropLast.js
@@ -0,0 +1,4 @@
+import dropRight from "./dropRight.js";
+export default function dropLast(iter) {
+  return dropRight(1, iter);
+}
--- Strict/dropRight.js
+++ Strict/dropRight.js
@@ -3,6 +3,6 @@ import takeAll from "./takeAll.js";
 import go1 from "./go1.js";
 import take from "./take.js";
 
-export default curry(function drop(l, iter) {
+export default curry(function dropRight(l, iter) {
   return go1(takeAll(iter), arr => take(arr.length - l, arr));
 });
