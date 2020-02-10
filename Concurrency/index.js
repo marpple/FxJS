b1365d83ecbea038b5a28ddeb29a6b2a79bad941
dohyeong <dhl@marpple.com>
2020-02-10 18:22:38 +0900

    누락된 export문 추가, 순서 정렬

--- Concurrency/index.js
+++ Concurrency/index.js
@@ -5,6 +5,7 @@ export { default as every } from './everyC.js';
 export { default as filter } from './filterC.js';
 export { default as find } from './findC.js';
 export { default as head } from './headC.js';
+export { default as juxt } from './juxtC.js';
 export { default as map } from './mapC.js';
 export { default as mapEntries } from './mapEntriesC.js';
 export { default as object, default as fromEntries } from "./objectC.js";
