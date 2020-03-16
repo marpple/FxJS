e0f8ead2a4e9723545252b2cbbcdd39132155f17
dohyeong <dhl@marpple.com>
2020-03-16 15:31:44 +0900

    apply함수 go함수 의존성 제거

--- Strict/apply.js
+++ Strict/apply.js
@@ -1,6 +1,5 @@
 import curry from './curry.js';
-import go1 from "./go1.js";
 
 export default curry(function apply(f, iter) {
-  return go1(iter, iter => f(...iter));
+  return f(...iter);
 });
--- Strict/both.js
+++ Strict/both.js
@@ -2,7 +2,8 @@ import apply from "./apply.js";
 import curry2 from "./curry2.js";
 import and from "./and.js";
 import juxt from "./juxt.js";
+import go from "./go.js";
 
 export default curry2(function both(f1, f2, ...args) {
-  return apply(and, juxt(f1, f2)(...args));
+  return go(args, apply(juxt(f1, f2)), apply(and));
 });
