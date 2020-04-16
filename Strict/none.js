40e1697387be024d3ee4806744a850657cff5d10
dohyeong <dhl@marpple.com>
2020-04-16 16:15:33 +0900

    none함수 비동기 제어 되도록 수정

--- Strict/none.js
+++ Strict/none.js
@@ -1,3 +1,6 @@
 import find from "./find.js";
-const none = (f, iter) => find(f, iter) === undefined;
+import go1 from "./go1.js";
+import isUndefined from "./isUndefined.js";
+
+const none = (f, iter) => go1(find(f, iter), isUndefined);
 export default none;
