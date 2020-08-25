e68210e27e5e4ffb110d5d52ab9687ce05d48445
daengdaenglee <gunho1020@gmail.com>
2020-08-25 19:10:47 +0900

    String 일떄 지원

--- Strict/last.js
+++ Strict/last.js
@@ -1,8 +1,9 @@
+import isString from "./isString.js";
 import reduce from "./reduce.js";
 import isArray from "./isArray.js";
 
 export default function last(iter) {
-  if (isArray(iter)) {
+  if (isArray(iter) || isString(iter)) {
     return iter[iter.length - 1];
   }
   return reduce((_, a) => a, iter);
