8c7b9824e7effb0a842950aa8fd35475c0835d05
Dohyeong <panamera1594@gmail.com>
2020-11-03 20:40:48 +0900

    esm 환경 babel target을 node 12버전으로 수정

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,8 +3,8 @@ module.exports = (api) => {
   const targets =
     BABEL_ENV === "cjs"
       ? { node: 6 }
-      : BABEL_ENV === "mjs"
-      ? { node: 10 }
+      : BABEL_ENV === "mjs" // node 10 버전에서 esm 사용하려면 확장자가 반드시 .mjs
+      ? { node: 12 }
       : BABEL_ENV === "modern"
       ? ">= 2% and last 2 versions"
       : { ie: 11 };
